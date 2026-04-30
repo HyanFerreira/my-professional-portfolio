b87e37ce169ae751f148219ec5bcfbe94fbc58e2
Hyan Ferreira <hcgamesmg2016@gmail.com>
2026-04-30 16:57:16 -0300

    add(data): adicionar itens de navegação ao projeto

---
+++ src/data/navigation.ts
@@ -0,0 +1,32 @@
+import type { NavigationItem } from "@/types/navigation";
+
+export const navigationItems: NavigationItem[] = [
+  {
+    label: "Início",
+    href: "/#inicio",
+  },
+  {
+    label: "Serviços",
+    href: "/#servicos",
+  },
+  {
+    label: "Projetos",
+    href: "/#projetos",
+  },
+  {
+    label: "Processo",
+    href: "/#processo",
+  },
+  {
+    label: "Tecnologias",
+    href: "/#tecnologias",
+  },
+  {
+    label: "Sobre",
+    href: "/#sobre",
+  },
+  {
+    label: "Contato",
+    href: "/#contato",
+  },
+];

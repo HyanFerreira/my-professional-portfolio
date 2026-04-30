2732c1e9f15dfe17e452e4e22f6abc24a411a9dc
Hyan Ferreira <hcgamesmg2016@gmail.com>
2026-04-30 16:55:23 -0300

    refactor(page): reorganizar estrutura do componente Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-
+import { AboutSection } from "@/components/sections/AboutSection";
+import { ContactSection } from "@/components/sections/ContactSection";
+import { FeaturedProjectsSection } from "@/components/sections/FeaturedProjectsSection";
+import { HeroSection } from "@/components/sections/HeroSection";
+import { ProcessSection } from "@/components/sections/ProcessSection";
+import { ServicesSection } from "@/components/sections/ServicesSection";
+import { SkillsSection } from "@/components/sections/SkillsSection";
 
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <>
+      <HeroSection />
+      <ServicesSection />
+      <FeaturedProjectsSection />
+      <ProcessSection />
+      <SkillsSection />
+      <AboutSection />
+      <ContactSection />
+    </>
   );
 }
